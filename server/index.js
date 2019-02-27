15cf79029404323ff3a787b8c2e7995d9221bb69
Mina Sorsok <mina.sorsok@gmail.com>
2019-02-26 16:14:15 -0800

    Update ec2 url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 80;
 
 const productDetailsServer =
-  'http://ec2-18-222-132-127.us-east-2.compute.amazonaws.com:3002';
+  'ec2-18-216-119-172.us-east-2.compute.amazonaws.com:3002';
 const ratingsServer =
   'http://ec2-18-217-232-247.us-east-2.compute.amazonaws.com:3003';
 const similarServer = 'http://ec2-18-212-94-82.compute-1.amazonaws.com:3004';
